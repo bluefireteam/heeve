ad8cc9e6eddcd4e401d4caf35e938d20ea4289e4
Lukas Klingsbo <me@lukas.fyi>
2021-11-06 14:14:38 +0100

    SelectionRectangle for selecting units

--- lib/heeve_game.dart
+++ lib/heeve_game.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
+import 'package:flame/palette.dart';
 
 class HeeveGame extends FlameGame with PanDetector {
   @override
@@ -12,8 +15,41 @@ class HeeveGame extends FlameGame with PanDetector {
     add(SpriteComponent(sprite: backgroundSprite));
   }
 
+  SelectionRectangle selection = SelectionRectangle();
+
+  @override
+  void onPanStart(DragStartInfo info) {
+    selection.position = info.eventPosition.viewportOnly;
+    selection.size.setZero();
+    add(selection);
+  }
+
   @override
   void onPanUpdate(DragUpdateInfo info) {
-    camera.moveTo(camera.position - info.delta.global);
+    //camera.moveTo(camera.position - info.delta.global);
+    selection.size += info.delta.viewportOnly;
+  }
+
+  @override
+  void onPanCancel() {
+    remove(selection);
+  }
+
+  @override
+  void onPanEnd(DragEndInfo _) {
+    onPanCancel();
+  }
+}
+
+class SelectionRectangle extends PositionComponent {
+  @override
+  bool isHud = true;
+
+  final paint = BasicPalette.red.paint()..style = PaintingStyle.stroke;
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawRect(size.toRect(), paint);
   }
 }
