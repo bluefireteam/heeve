06b52cd6eec3bf9aceac8d4f22376f864aa73065
Luan Nico <luanpotter27@gmail.com>
2021-11-07 22:32:52 -0500

    Add isometric map & block selector

--- lib/heeve_game.dart
+++ lib/heeve_game.dart
@@ -3,53 +3,51 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
-import 'package:flame/palette.dart';
+import 'package:flame/sprite.dart';
 
-class HeeveGame extends FlameGame with PanDetector {
+import 'map_generator.dart';
+import 'selector.dart';
+
+class HeeveGame extends FlameGame
+    with MouseMovementDetector, ScrollDetector, PanDetector, KeyboardEvents {
+  late final IsometricTileMapComponent map;
+  late final Selector selector;
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     camera.speed = 5000;
     camera.viewport = FixedResolutionViewport(Vector2(800, 600));
-    final backgroundSprite = await loadSprite('background.jpg');
-    add(SpriteComponent(sprite: backgroundSprite));
+
+    final tileset = SpriteSheet(
+      image: await images.load('tileset.png'),
+      srcSize: Vector2.all(32.0),
+    );
+    final matrix = MapGenerator.generateMap();
+    add(map = IsometricTileMapComponent(tileset, matrix));
+    add(selector = Selector(tileset.getSprite(3, 3)));
   }
 
-  SelectionRectangle selection = SelectionRectangle();
+  @override
+  void onMouseMove(PointerHoverInfo details) {
+    final pos = details.eventPosition.game;
+    final block = map.getBlock(pos);
+    if (map.containsBlock(block)) {
+      selector.block = block;
+    }
+  }
 
   @override
-  void onPanStart(DragStartInfo info) {
-    selection.position = info.eventPosition.viewportOnly;
-    selection.size.setZero();
-    add(selection);
+  void onScroll(PointerScrollInfo event) {
+    final zooms = [0.25, 0.5, 1.0, 2.0, 4.0, 10.0];
+    final idx =
+        zooms.indexOf(camera.zoom) - event.scrollDelta.game.y.sign.toInt();
+    camera.zoom = zooms[idx % zooms.length];
   }
 
   @override
   void onPanUpdate(DragUpdateInfo info) {
-    //camera.moveTo(camera.position - info.delta.global);
-    selection.size += info.delta.viewportOnly;
-  }
-
-  @override
-  void onPanCancel() {
-    remove(selection);
-  }
-
-  @override
-  void onPanEnd(DragEndInfo _) {
-    onPanCancel();
-  }
-}
-
-class SelectionRectangle extends PositionComponent {
-  @override
-  bool isHud = true;
-
-  final paint = BasicPalette.red.paint()..style = PaintingStyle.stroke;
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    canvas.drawRect(size.toRect(), paint);
+    camera.translateBy(info.delta.global * -1);
+    camera.snap();
   }
 }
